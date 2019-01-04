e14d8340aac6c338db03d5f6272436fcbf2c642f
FrankieBoye <44870179+FrankieBoye@users.noreply.github.com>
2019-01-04 23:00:59 +0000

    Create Car case.rb

---
+++ Car case.rb
@@ -0,0 +1,17 @@
+puts "What's your favorite car marque?"
+marque = gets.chomp
+
+case marque
+when "Mercedes"
+  puts "The best or nothing!"
+when "Ford"
+  puts "The car you always promised yourself."
+when "Jaguar"
+  puts "Grace, space and pace."
+when "Honda"
+  puts "The power of dreams."
+when  "Aston Martin"
+  puts "Power, Beauty and Soul."
+else
+  puts "I don't know that marque!"
+end
